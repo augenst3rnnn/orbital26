05ab43117c90ee5470fa148492becb7b55af4337
Woo Yu Xin <240400163+augenst3rnnn@users.noreply.github.com>
2026-05-16 19:41:11 +0800

    Remove cookedlah folder from GitHub

--- cookedlah/backend/src/config/env.js
+++
@@ -1,9 +0,0 @@
-//centralise all environment variables in one file
-import "dotenv/config"; //to use .env file
-
-export const ENV = {
-    PORT: process.env.PORT || 5001,
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
-};
-
