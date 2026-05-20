53db924c93682cf81d0da2a589770debf9e21e55
hello123565 <katelsalamante@gmail.com>
2026-05-20 15:53:04 +0800

    feat: Add Firestore user profile management

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey:  "AIzaSyDbPxqprxL9kWzlxwxO-vzknEBxVQmmJto",
@@ -16,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
+export const db = getFirestore(app);
