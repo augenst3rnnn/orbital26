cbb2b1e4bc536e2f788480396c1ae4541e7cbae4
Woo Yu Xin <240400163+augenst3rnnn@users.noreply.github.com>
2026-05-22 22:38:21 +0800

    basic layout of homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 
-import { View, Text, TouchableOpacity} from 'react-native';
+import { View, Text, TouchableOpacity,Image, StatusBar, ScrollView, TextInput} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { signOut } from 'firebase/auth';
 import { auth } from '../config/firebase';
@@ -35,8 +35,40 @@ export default function HomeScreen() {
   };
 
   return (
-    <SafeAreaView>
-      <Text className="text-lg" >Home Page</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className='flex-1 bg-white'>
+        <StatusBar style="dark" />
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{paddingBottom: 50}}
+          className="space-y-6 pt-2"
+        >
+        
+        {/*avatar and bell icon*/}
+        <View className="px-4 flex-row justify-between items-center mb-2">
+          <Image source={require('../assets/images/avatar.png')} style={{height: 50, width: 50}} />
+          <Image source={require('../assets/images/bell.png')} style={{height: 30, width: 30}} />
+        </View>
+
+        {/*greetings*/}
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{fontSize: 20, fontWeight: "bold"}}>Good morning {userData ? userData.displayName : "User"}!</Text>
+          <Text style={{fontSize: 30}} className="font-semibold text-neutral-600 mt-1">What would you like to cook today?</Text>
+        </View>
+
+        {/*search bar*/}
+        <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
+          <TextInput
+            placeholder='Search any recipes'
+            placeholderTextColor={'gray'}
+            className="flex-1 text-base mb-1 pl-2"
+          />
+          <View className="bg-white rounded-full p-3">
+            <Image source={require('../assets/images/search.png')} style={{height: 20, width: 20}} />
+          </View>
+        </View>
+        </ScrollView>  
+      </View>  
 
       {loading ? (
         <Text>Loading...</Text>
