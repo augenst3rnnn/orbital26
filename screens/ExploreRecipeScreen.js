5efcb8fa7e4b3790cfe3aa7454ffa34cb85db130
hello123565 <katelsalamante@gmail.com>
2026-06-08 16:52:11 +0800

    simple recipe modal popup

--- screens/ExploreRecipeScreen.js
+++ screens/ExploreRecipeScreen.js
@@ -9,6 +9,7 @@ import {
   TextInput,
   Pressable,
   FlatList,
+  Modal,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { mockRecipes } from "../data/mockRecipes";
@@ -16,6 +17,8 @@ import { mockRecipes } from "../data/mockRecipes";
 export default function ExploreRecipeScreen() {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedType, setSelectedType] = useState("all");
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const categories = ["all", "breakfast", "main course", "snack", "dessert"];
 
@@ -29,7 +32,13 @@ export default function ExploreRecipeScreen() {
 
   const renderRecipeCard = ({ item }) => {
     return (
-      <TouchableOpacity className="bg-white rounded-lg shadow-md p-4 mb-4">
+      <TouchableOpacity
+        className="bg-white rounded-lg shadow-md p-4 mb-4"
+        onPress={() => {
+          setSelectedRecipe(item);
+          setModalVisible(true);
+        }}
+      >
         <Image source={item.image} className="w-full h-44" resizeMode="cover" />
         <View className="mt-2">
           <Text className="text-lg font-semibold">{item.title}</Text>
@@ -49,9 +58,9 @@ export default function ExploreRecipeScreen() {
       <FlatList
         data={filteredRecipes}
         keyExtractor={(item) => item.id.toString()}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
         renderItem={renderRecipeCard}
-        contentContainerStyle={{ padding: 16 }}
+        contentContainerStyle={{ padding: 18 }}
         ListHeaderComponent={
           <View>
             <Text className="text-3xl font-bold text-gray-900 mb-2">
@@ -96,22 +105,32 @@ export default function ExploreRecipeScreen() {
           </View>
         }
       />
+
+      <Modal visible={modalVisible} transparent={true} animationType="fade">
+        <View className="flex-1 bg-gray-200 bg-opacity-10 p-10 justify-center items-center">
+          <View className="bg-white rounded-3xl p-10 w-full w-11/12 shadow-lg">
+            <Image
+              source={selectedRecipe?.image}
+              className="w-full h-64 rounded-lg pb-2"
+              resizeMode="cover"
+            />
+            <Text className="text-2xl font-bold">{selectedRecipe?.title}</Text>
+            <Text className="text-sm text-gray-500 mb-4">
+              {selectedRecipe?.summary}
+            </Text>
+            <View className="bg-yellow-300 rounded-lg p-4 mb-4">
+              <Text className="text-lg font-semibold mb-2">ingredients</Text>
+              <Text className="text-gray-700 mb-4">
+                {selectedRecipe?.usedIngredientCount} available,{" "}
+                {selectedRecipe?.missedIngredientCount} missing
+              </Text>
+            </View>
+            <Pressable onPress={() => setModalVisible(false)}>
+              <Text className="underline">Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
-
-/*
-//placeholder first
-
-export default function ExploreRecipeScreen() {
-  return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-xl font-semibold text-gray-800">
-          Explore Recipes
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
-}
-  */
