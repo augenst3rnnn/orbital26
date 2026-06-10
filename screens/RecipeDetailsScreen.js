b1ce15b1ce36b76656a47ad7ff3867429f090f5f
hello123565 <katelsalamante@gmail.com>
2026-06-10 20:13:36 +0800

    WIP recipe details screen UI

--- screens/RecipeDetailsScreen.js
+++ screens/RecipeDetailsScreen.js
@@ -1,27 +1,29 @@
-import { View, Text, ScrollView, Pressable } from "react-native";
+import { View, Text, ScrollView, Pressable, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-{
-  /*placeholder first*/
-}
-
-export default function RecipeDetailsScreen({ navigation }) {
+export default function RecipeDetailsScreen({ route, navigation }) {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-xl font-semibold text-gray-800">
-          Recipe Details
-        </Text>
-
-        <Pressable
-          onPress={() => navigation.goBack()}
-          className="mt-4 bg-yellow-300 px-4 py-2 rounded-lg"
-        >
-          <Text className="text-gray-800 font-semibold">
-            Return to Explore Page
-          </Text>
-        </Pressable>
+    //fix this UI
+    <ScrollView
+      className="flex-1 bg-white rounded-lg"
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ padding: 2 }}
+    >
+      <View className="p-1">
+        <Image
+          source={route.params.recipe.image}
+          className="w-full h-[400px] rounded-t-3xl"
+          resizeMode="cover"
+        />
       </View>
-    </SafeAreaView>
+
+      {/*back button*/}
+      <Pressable
+        className="absolute top-10 left-5 bg-white rounded-full p-2 shadow"
+        onPress={() => navigation.goBack()}
+      >
+        <Text className="text-black text-xl">←</Text>
+      </Pressable>
+    </ScrollView>
   );
 }
