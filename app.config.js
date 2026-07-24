71c62a0c72efc116dc42ec489d265d0798dd34e7
Woo Yu Xin <240400163+augenst3rnnn@users.noreply.github.com>
2026-07-24 09:44:42 +0800

    security: move API key to .env, add app.config.js

---
+++ app.config.js
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "frontend",
+    slug: "frontend",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: { supportsTablet: true },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/android-icon-foreground.png",
+        backgroundImage: "./assets/android-icon-background.png",
+        monochromeImage: "./assets/android-icon-monochrome.png"
+      },
+      package: "com.soyabae.frontend"
+    },
+    web: { favicon: "./assets/favicon.png" },
+    extra: {
+      SPOONACULAR_API_KEY: process.env.SPOONACULAR_API_KEY,
+      eas: { projectId: "edf188d2-5add-44c5-96a0-43a313ed3199" }
+    }
+  }
+};
