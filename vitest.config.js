634b697d0d4d7d12b29e5d3d9aaa405fd0941a2a
hello123565 <katelsalamante@gmail.com>
2026-07-26 10:42:15 +0800

    unit tests run by npm run test, integration tests run by npm run test:integration, npm run test:all runs both sequentially

--- vitest.config.js
+++ vitest.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "node",
-    include: ["**/tests/**/*.test.{js,jsx}"],
     globals: true,
+    environment: "node",
+
+    include: ["**/UnitTests/**/*.test.{js,jsx}"],
+
+    exclude: ["tests/IntegrationTests/**", "node_modules/**"],
+
     /*transform: {
       "^.+\\.(js|jsx)$": "babel-jest",
     },*/
