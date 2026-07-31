6362137ff91c323f7d462d7fc9102dd4be79274c
hello123565 <katelsalamante@gmail.com>
2026-07-31 10:25:03 +0800

    tested workflow for saving a recipe by clicking on heart button in recipe details screen

--- tests/IntegrationTests/ExploreRecipeScreen.integration.test.jsx
+++ tests/IntegrationTests/ExploreRecipeScreen.integration.test.jsx
@@ -152,4 +152,70 @@ describe("ExploreRecipeScreen integration", () => {
       recipe: recipeSearchResult,
     });
   });
+
+  //include expect call to getRecipeDetails when navigating to RDS?
+  it("adds recipe to favourites from details screen", async () => {
+    const recipeDetails = {
+      id: 716429,
+      title: "Pasta with Garlic",
+      image: "https://example.com/pasta.jpg",
+      readyInMinutes: 30,
+      servings: 2,
+      summary: "A simple pasta recipe.",
+      ingredients: [
+        "200g pasta",
+        "2 cloves garlic",
+        "1 tbsp olive oil",
+        "50g butter",
+        "1 pinch salt",
+      ],
+      instructions: ["Boil pasta", "Chop and mix in garlic", "Cook pasta"],
+      likes: 3,
+    };
+
+    //define that the recipe isnt alr saved in user's favs
+    getFavoriteRecipes.mockResolvedValueOnce([]);
+
+    //saveFavoriteRecipe.mockResolvedValueOnce();
+
+    const navigation = {
+      goBack: jest.fn(),
+      navigate: jest.fn(),
+    };
+
+    const fakeRoute = {
+      params: {
+        recipe: recipeDetails,
+      },
+    };
+
+    const user = userEvent.setup();
+
+    await render(
+      <RecipeDetailsScreen navigation={navigation} route={fakeRoute} />,
+    );
+
+    await waitFor(() => {
+      expect(getFavoriteRecipes).toHaveBeenCalledWith("test-user-123");
+    });
+
+    //user clicks heart button
+    await user.press(screen.getByTestId("favourite-button"));
+
+    await waitFor(() => {
+      expect(saveFavoriteRecipe).toHaveBeenCalledWith(
+        "test-user-123",
+        716429,
+        expect.objectContaining({
+          id: 716429,
+          title: "Pasta with Garlic",
+          image: "https://example.com/pasta.jpg",
+          isMock: false,
+        }),
+      );
+    });
+
+    expect(await screen.findByTestId("favorite-heart-filled")).toBeTruthy();
+    expect(screen.queryByTestId("favorite-heart-unfilled")).toBeNull();
+  });
 });
